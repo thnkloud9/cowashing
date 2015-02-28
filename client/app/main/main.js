c3d5a99e35df611c34c7621bee399c1c247e7cf8
Mark Lewis <thnkloud9@gmail.com>
2015-02-28 09:38:05 +0100

    initial commit yeoman generated code

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cowashingApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
